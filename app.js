84b13a82464f6307dbc9650a4c2f4e4e23479932
neil-dahiya <neil.dahiya@rapido.bike>
2021-04-30 11:03:12 +0530

    counter added

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const fs = require("fs");
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+    const data = fs.readFileSync('count.txt', {encoding:'utf8', flag:'r'})
+    console.log(data);
+    res.json({calls: data})
+})
+
+app.post('/incCount', (req, res)=>{
+    const data = fs.readFileSync('count.txt', {encoding:'utf8', flag:'r'})
+    console.log(parseInt(data)+1);
+    const ans = parseInt(data)+1
+    fs.writeFileSync("count.txt", ans);
+    res.json({calls: ans})
+})
+
+app.put('reset', (req, res)=>{
+    fs.writeFileSync("count.txt", 0);
+    res.json({calls: 0})
+})
+
+app.listen(3000, ()=>{
+    console.log("Started server");
+})
